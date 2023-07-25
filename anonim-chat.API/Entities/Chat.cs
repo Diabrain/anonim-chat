3da219a3f237a2889119bfa11c6b51505bba79c5
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-25 14:55:10 +0500

    add

---
+++ anonim-chat.API/Entities/Chat.cs
@@ -0,0 +1,8 @@
+﻿namespace anonim_chat.API.Entities;
+
+public class Chat
+{
+    public Guid Id { get; set; }
+    public required string Key { get; set; }
+    
+}
---
+++ anonim-chat.API/Entities/User.cs
@@ -0,0 +1,7 @@
+﻿namespace anonim_chat.API.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public required string UserName { get; set; }
+}

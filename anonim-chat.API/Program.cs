a6388dbef8f7bff1a08358080516a8bd0eb0e681
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-28 16:00:08 +0500

    update-entities

--- anonim-chat.API/Program.cs
+++ anonim-chat.API/Program.cs
@@ -1,10 +1,38 @@
+using anonim_chat.API.Context;
+using anonim_chat.API.Extensions;
+using anonim_chat.API.Managers;
+using anonim_chat.API.Middleware;
+using anonim_chat.API.Models;
+using anonim_chat.API.Validators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGenJwt();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSnakeCaseNamingConvention()
+        .UseInMemoryDatabase("chats");
+
+});
+
+
+
+builder.Services.AddJwt(builder.Configuration);
+
+builder.Services.AddScoped<IValidator<CreateUserModel>, CreateUserValidator>();
+builder.Services.AddScoped<IChatManager, ChatManager>();
+builder.Services.AddScoped<IUserProvider, UserProvider>();
+builder.Services.AddScoped<IUserManager, UserManager>();
+builder.Services.AddScoped<IJwtTokenManager, JwtTokenManager>();
+builder.Services.AddHttpContextAccessor();
+
+
 
 var app = builder.Build();
 
@@ -15,9 +43,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
+app.UseErrorHandlerMiddleware();
 app.MapControllers();
-
 app.Run();
